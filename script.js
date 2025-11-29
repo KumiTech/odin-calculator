de37dc283d30dcc865fb7c071c783ce33e34655b
Kumi <gyabaahkelvin27@gmail.com>
2025-11-29 23:06:02 +0000

    Implemented Logic for Calculator

--- script.js
+++ script.js
@@ -13,48 +13,110 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    return a/b
+    if( b === "0"){
+     updateDisplay("Cannot divide a number by zero ")
+     clearDisplay()
+     return 0
+    }
+    {
+     return a/b
+    }
 }
 
-// Variables for operations
-let firstNum
-let secondNum
-let operator
 
 //Operate Function 
 function operate(firstNum,secondNum,operator){
     switch (operator) {
         case '+':
-            add(firstNum,secondNum)
-        break;
+            return add(firstNum,secondNum)
         case '-':
-            subtract(firstNum,secondNum)
-        break;
-        case '*':
-            multiply(firstNum,secondNum)
-        break;
-        case '/':
-            divide(firstNum,secondNum)
-        break;
+           return subtract(firstNum,secondNum)
+        case '×':
+           return multiply(firstNum,secondNum)
+        case '÷':
+        return divide(firstNum,secondNum)
     }
 }
 
 const display = document.querySelector('#display')
-
 const numBtns = document.querySelectorAll(".number")
+const operatorBtns = document.querySelectorAll(".operator")
+const equalBtn = document.querySelector(".equals")
+const clearBtn = document.querySelector(".clear")
 
-let displayText = ""
 
-numBtns.forEach( btn => {
-    btn.addEventListener('click', (e) => {
-        const numValue = e.target.textContent;
-        displayText += numValue
-        display.value = displayText  
+
+// Variables for operations
+let firstNum = ""
+let secondNum = ""
+let currentOperator = null
+let shouldResetDisplay = false
+
+
+// Function to update display
+function updateDisplay(value) {
+    display.value = value || '0'
+}
+
+//function to reset display 
+function resetDisplay(){
+    display.value = ""
+    shouldResetDisplay = false
+}
+
+// function to clear display
+function clearDisplay(){
+firstNum = ""
+secondNum = ""
+currentOperator = null
+shouldResetDisplay = false
+updateDisplay("0")
+
+}
+
+clearBtn.addEventListener('click', clearDisplay)
+
+
+  numBtns.forEach( btn => {
+        btn.addEventListener('click', () => {
+            if(shouldResetDisplay){
+                resetDisplay()
+            }
+            display.value += btn.textContent 
+        })
+    })
+
+// operator button
+operatorBtns.forEach( btn => {
+    btn.addEventListener('click',() => {
+        const clickedOperator = btn.textContent
+
+        // Logic for when there is already a number
+        if(firstNum && !shouldResetDisplay){
+            secondNum = parseFloat(display.vlaue)
+            const result = operate(parseFloat(firstNum),secondNum,currentOperator)
+            updateDisplay(result)
+            firstNum = result
+        }
+        else{
+            firstNum = parseFloat(display.value)
+        }
+        currentOperator = clickedOperator
+        shouldResetDisplay = true
     })
 })
 
- 
+// equals
+equalBtn.addEventListener('click', () => {
+    if(!firstNum || !currentOperator) 
+        {return;}
+    secondNum = parseFloat(display.value)
+    const result = operate(parseFloat(firstNum),secondNum,currentOperator)
+    updateDisplay(result)
 
+    firstNum = result
+    currentOperator = null 
+    shouldResetDisplay = true
+})
 
-
-
+updateDisplay("0")
